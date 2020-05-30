d8be8ce6e2fb2d810c5fea96ee9cb6eb7f5169f9
MasterVisput <55348522+MasterVisput@users.noreply.github.com>
2020-05-30 13:23:16 +0300

    решение базового задания DZ13

--- Test_suite/tests/test_add_file.py
+++ Test_suite/tests/test_add_file.py
@@ -1,13 +1,8 @@
 from time import sleep
 
 class TestAddFile:
-    def test_add_file(self, browser, input_page):
+    def test_add_file(self,input_page):
         path = 'K:/Learn/POM_Learning_OTUS/Test_suite/data/pict.jpg'
         input_page.load_file(path=path)
-
-    def test_add_file_to_opencart(self, browser, admin_product_page):
-        path = 'K:/Learn/POM_Learning_OTUS/Test_suite/data/pict.jpg'
-        sleep(5)
-
 
 
